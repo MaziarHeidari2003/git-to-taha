c78d5fd9e4fc3b79b815f884430be45fe7424fa9
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-11-20 09:50:52 -0800

    quiz _game_added

---
+++ quiz_game.py
@@ -0,0 +1,65 @@
+#------------------------
+def new_game():
+  guesses = []
+  correct_guesses = 0 
+  question_num = 1
+
+  for key in questions:
+    print("#----------------------")
+    print(key)
+    for i in options[question_num-1]:
+      print(i)
+    guess = input("Enter A,B,C or D: ").upper()
+    guesses.append(guess)  
+    correct_guesses += check_answer(questions.get(key),guess)
+    question_num +=1  
+
+  display_score(correct_guesses,guesses)  
+#------------------------
+
+def check_answer(answer,guess):
+  if guess == answer :
+    print("CORRECT")
+    return 1
+  else:
+    print("WRONG")
+    return 0
+  
+#------------------------
+
+def display_score(correct_guesses,guesses):
+  print("---------------------")
+  print("RESULT")
+  print("----------------------")
+  print("Answers: ",end="")
+  for i in questions:
+    print(questions.get(i), end=" ")
+  print()  
+  print("Guesses: ",end="")
+  for i in guesses:
+    print(i, end=" ")
+  print()  
+  
+#------------------------
+
+def play_again():
+  pass
+#------------------------
+
+
+questions = {
+  "Who created python? ": "A",
+  "What year was python created? ": "B",
+  "Python is tributed to wich comedi group? ": "C",
+  "Is the earth round? ": "A"
+}
+
+options = [
+  ["A. Ali Daie", "B. Adel Ferdosipoor", "C. Mark Zucjerburg"],
+  ["A. 1999", "B. 1991", "C. 2000","D. 2018"],
+  ["A. lonely iland","B. smooth", "C. monty python", "D. SNL"],
+  ["A. True","B. False", "C. sometimes","D. whats earth?"]
+]
+
+new_game()
+
